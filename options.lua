ae433786ea0999a8d020ab35e01bf18b875e5d40
Shrugtal <14936758+Shrugtal@users.noreply.github.com>
2019-09-27 00:15:44 +0100

    Ordered Options Menu

--- options.lua
+++ options.lua
@@ -85,7 +85,7 @@ end
 function GenerateSpellOptionsForCategory (categoryFrame, key)
 	local array = GetSpellList()
 	local pos = -16
-	for spell, pair in pairs(array[key]) do
+	for spell, pair in orderedPairs(array[key]) do
 		local spellName = spell
 		local checkBtn = CreateFrame("CheckButton", "cb", categoryFrame, "UICheckButtonTemplate");
 		checkBtn:SetPoint("TOPLEFT", 0, pos);
@@ -117,4 +117,51 @@ function GenerateSpellOptionsForCategory (categoryFrame, key)
 	categoryFrame:SetSize(300, count)
 	totalSize = totalSize + count
 	return count
+end
+
+--utility functions from lua-users.org
+
+function __genOrderedIndex( t )
+    local orderedIndex = {}
+    for key in pairs(t) do
+        table.insert( orderedIndex, key )
+    end
+    table.sort( orderedIndex )
+    return orderedIndex
+end
+
+function orderedNext(t, state)
+    -- Equivalent of the next function, but returns the keys in the alphabetic
+    -- order. We use a temporary ordered key table that is stored in the
+    -- table being iterated.
+
+    local key = nil
+    --print("orderedNext: state = "..tostring(state) )
+    if state == nil then
+        -- the first time, generate the index
+        t.__orderedIndex = __genOrderedIndex( t )
+        key = t.__orderedIndex[1]
+    else
+        -- fetch the next value
+        for i = 1,table.getn(t.__orderedIndex) do
+            if t.__orderedIndex[i] == state then
+                key = t.__orderedIndex[i+1]
+            end
+        end
+    end
+
+    if key then
+		print(key)
+        return key, t[key]
+    end
+
+    -- no more value to return, cleanup
+    t.__orderedIndex = nil
+    return
+end
+
+function orderedPairs(t)
+    -- Equivalent of the pairs() function on tables. Allows to iterate
+    -- in order
+    return orderedNext, t, nil
 end
